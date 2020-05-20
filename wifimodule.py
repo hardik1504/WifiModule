c8c5c7353e803933f7feb55ce1b3e6444586e8aa
Hardik Sharma <65143877+hardik1504@users.noreply.github.com>
2020-05-20 05:45:47 +0530

    Update wifimodule.py

--- wifimodule.py
+++ wifimodule.py
@@ -13,6 +13,18 @@ def turn_wifi_on():
     return [cmd.returncode]
 
 
+def is_connected(ssid):
+    args = ["/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport", "-I", "|", "grep SSID"]
+    cmd = subprocess.run(args=args, universal_newlines=True, stdout=subprocess.PIPE)
+    stdout = cmd.stdout.splitlines()
+    connected = False
+    for item in stdout:
+        if item.find(ssid) != -1:
+            connected = True
+            break
+    return [cmd.returncode, connected]
+
+
 def scan_wifi(ssid):
     args = ["/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport", "scan"]
     cmd = subprocess.run(args=args, universal_newlines=True, stdout=subprocess.PIPE)
@@ -34,5 +46,6 @@ def connect_wifi(ssid, passw):
 if __name__ == "__main__":
     print(turn_wifi_off())
     print(turn_wifi_on())
-    print(scan_wifi("TP-Link_133A"))
-    print(connect_wifi("TP-Link_133A", "hardiksharma"))
+    print(scan_wifi("SSID"))
+    print(connect_wifi("SSID", "PASSWORD"))
+    print(is_connected("SSID"))
